e12bdb88081bcfd0301d64ba8299c2825b72706b
Leander Masopust <leandermasopust@Leanders-Mini.fritz.box>
2022-02-15 11:37:29 +0100

    add parallelization of seamMap

--- SeamCarving/ViewController.swift
+++ SeamCarving/ViewController.swift
@@ -10,6 +10,22 @@ import CoreImage.CIFilterBuiltins
 import UIKit
 import CoreFoundation
 
+
+public class SafeArray {
+    private var array: [CGFloat] = []
+    private let accessQueue = DispatchQueue(label: "SynchronizedArrayAccess", attributes: .concurrent)
+
+    init(repeating: CGFloat, count: Int) {
+            array = Array(repeating: repeating, count: count)
+        }
+
+    public func append(_ newElement: CGFloat) {
+        self.accessQueue.async(flags:.barrier) {
+            self.array.append(newElement)
+        }
+    }
+}
+
 //https://stackoverflow.com/questions/24755558/measure-elapsed-time-in-swift
 class ParkBenchTimer {
 
@@ -79,6 +95,26 @@ class EnergyMapFilter: CIFilter {
         let url = Bundle.main.url(forResource: "default", withExtension: "metallib")!
         let data = try! Data(contentsOf: url)
         kernel = try! CIKernel(functionName: "energyMap", fromMetalLibraryData: data)
+        super.init()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func outputImage() -> CIImage? {
+        let sampler = CISampler.init(image:inputImage!)
+        guard let inputImage = inputImage else {return nil}
+        return kernel.apply(extent: inputImage.extent, roiCallback: {(index,rect)-> CGRect in return rect}, arguments: [sampler, inputImage.extent.width, inputImage.extent.height])
+    }
+}
+
+class SeamMapFilter: CIFilter {
+
+    private let kernel: CIKernel
+    var inputImage: CIImage?
+    override init() {
+        let url = Bundle.main.url(forResource: "default", withExtension: "metallib")!
+        let data = try! Data(contentsOf: url)
+        kernel = try! CIKernel(functionName: "seamMap", fromMetalLibraryData: data)
         super.init()
     }
     required init?(coder aDecoder: NSCoder) {
@@ -178,7 +214,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                     self.imageView.image =  UIImage(cgImage: self.img!)
                     self.imageView.setNeedsDisplay()
                 }
-                //showSeamMap(seamMap: seamMap)
+                //self.showSeamMap(seamMap: self.seamMap!)
             }
             print("The Carving took \(timer.stop()) seconds.")
         }
@@ -192,26 +228,96 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         return outputImage
     }
 
-    func calculateSeamMap(energyMap: CGImage!, lastSeam: [Int]?, lastSeamMap: inout [[CGFloat]]?){
-        if(lastSeamMap == nil || lastSeam == nil) {
+    func calculateSeamMapWithShader(energyMap: CGImage!, lastSeam: [Int]?, lastSeamMap: inout [[CGFloat]]?){
             var map = [[CGFloat]](repeating: [CGFloat](repeating: 0, count: Int(width)), count: Int(height))
             for y in 0...(height-1) {
-                for x in 0...(width-1) {
-                    let red = energyMap[x,y][0]
-
-                    if(x == 0 || x == (width-1)) {
-                        // workaround to avoid edge-cutting
-                        map[y][x] = CGFloat.greatestFiniteMagnitude
-                    }
-                    else if(y == 0) {
+                if(y == 0) {
+                    for x in 0...(width-1) {
+                        let red = energyMap[x,y][0]
                         map[y][x] = red
                     }
-                    else {
-                        map[y][x] = min(min(map[y-1][x-1], map[y-1][x]),map[y-1][x+1]) + red
-                    }
                 }
+                else {
+                    let seamMapFilter = SeamMapFilter()
+                    let colorSpace = CGColorSpaceCreateDeviceRGB()
+                    let bytesPerPixel:Int = 4
+                    let bytesPerRow = 4 * width
+                    let bitsPerComponent = 8
+                    let dataSize =  width * bytesPerPixel * 2
+                    var rawData = [UInt8](repeating: 0, count: Int(dataSize))
+                    let bitmapInfo = img!.bitmapInfo.rawValue
+                    let context = CGContext(data: &rawData, width: width, height: 2, bitsPerComponent: bitsPerComponent, bytesPerRow: bytesPerRow, space: colorSpace, bitmapInfo: bitmapInfo)!
+
+                    var byteIndex = 0
+                    // Iterate through pixels
+                    let maxV = lastSeamMap![y-1].max()! + 1
+                    while byteIndex < dataSize {
+
+                        // Get Column and Row of current pixel
+                        let column =  ((byteIndex / 4) % (bytesPerRow/4))
+                        let row = ((byteIndex - (column*4)) / bytesPerRow )
+                        if(row == 0) {
+                            rawData[byteIndex + 0] = UInt8(lastSeamMap![y-1][column] / maxV)
+                            rawData[byteIndex + 1] = UInt8(lastSeamMap![y-1][column] / maxV)
+                            rawData[byteIndex + 2] = UInt8(lastSeamMap![y-1][column] / maxV)
+                            rawData[byteIndex + 3] = UInt8(lastSeamMap![y-1][column] / maxV)
+                        }
+                        if(row == 1) {
+                            rawData[byteIndex + 0] = UInt8(energyMap[column, y][0] / maxV)
+                            rawData[byteIndex + 1] = UInt8(energyMap[column, y][0] / maxV)
+                            rawData[byteIndex + 2] = UInt8(energyMap[column, y][0] / maxV)
+                            rawData[byteIndex + 3] = UInt8(energyMap[column, y][0] / maxV)
+                        }
+                        byteIndex += 4
+                        continue
+                }
+                    seamMapFilter.inputImage = CIImage(cgImage: context.makeImage()!)
+                    let outputImage = CIContext().createCGImage(seamMapFilter.outputImage()!, from: seamMapFilter.inputImage!.extent)!
+                    for x in 0...(width-1) {
+                        lastSeamMap![y][x] = outputImage[x,0][0] * maxV
+                    }
             }
             lastSeamMap = map
+        }
+    }
+    var extr = 0.0
+    var calc = 0.0
+    func calculateSeamMapAt(x:Int, y:Int, energyMap: CGImage!, map: [[CGFloat]]) -> CGFloat {
+        let timer = ParkBenchTimer()
+        let a = timer.stop()
+        extr += a
+
+        if(x == 0 || x == (width-1)) {
+            // workaround to avoid edge-cutting
+            return CGFloat.greatestFiniteMagnitude
+        }
+        else if(y == 0) {
+            return energyMap[x,y][0]
+        }
+        else {
+            let timer2 = ParkBenchTimer()
+            let b = min(min(map[y-1][x-1], map[y-1][x]),map[y-1][x+1]) + energyMap[x,y][0]
+            calc += timer2.stop()
+            return b
+        }
+
+    }
+
+    func calculateSeamMap(energyMap: CGImage!, lastSeam: [Int]?, lastSeamMap: inout [[CGFloat]]?){
+        if(lastSeamMap == nil || lastSeam == nil) {
+            var map = [[CGFloat]]()
+            for y in 0..<height {
+                let buffer = Array<CGFloat>(unsafeUninitializedCapacity: width, initializingWith: { (subBuffer: inout UnsafeMutableBufferPointer<CGFloat>, subCount: inout Int) in
+                    DispatchQueue.concurrentPerform(iterations: width) { x in
+                        subBuffer[x] = calculateSeamMapAt(x: x, y: y, energyMap: energyMap, map: map)
+                    }
+                    subCount = width
+                })
+                map.append(buffer)
+            }
+            lastSeamMap = map
+            print(extr)
+            print(calc)
         }
         else {
             for y in 0...(height-1) {
@@ -266,7 +372,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                 rawData[byteIndex + 0] = UInt8(0)
                 rawData[byteIndex + 1] = UInt8(0)
                 rawData[byteIndex + 2] = UInt8(0)
-                rawData[byteIndex + 0] = UInt8(0)
+                rawData[byteIndex + 3] = UInt8(0)
                 byteIndex += 4
                 continue
             }
--- SeamCarving/kernel.metal
+++ SeamCarving/kernel.metal
@@ -13,4 +13,14 @@ extern "C" { namespace coreimage {
         float3 diffNegX = (pow(col - colNegX,2)).rgb;
         return float4(float3(sqrt(diffX.x + diffX.y + diffX.z + diffNegX.x + diffNegX.y + diffNegX.z)),1.);
     }
+    float4 seamMap(sampler s, float width, float height) {
+        float onePixelHeight = 1./height;
+        float onePixelWidth = 1./width;
+        float4 col = s.sample(s.coord());
+        float4 colY = s.sample(s.coord() + float2(0.0,onePixelHeight));
+        float4 colYX = s.sample(s.coord() + float2(onePixelWidth,onePixelHeight));
+        float4 colYNegX = s.sample(s.coord() - float2(onePixelWidth,0.0) + float2(0.0,onePixelHeight));
+        float val = min(min(colY.r, colYX.r), colYNegX.r);
+        return float4(float3(col+val),1.);
+    }
 }}
